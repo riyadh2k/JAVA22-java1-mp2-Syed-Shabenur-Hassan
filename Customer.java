6a8e1f49c8df67a1c5053e219b6bb611525fd6a9
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2022-10-16 12:26:58 +0200

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -10,17 +10,7 @@ class Customer {
     void totalPrice() {
         System.out.println(" Total product bought: "+initialProduct+" Total Cost: "+totalCost);
     }
-//    void printAll() {
-//    	printIt();
-//    }
 
-    int getProdAmount() {
-        return this.initialProduct;
-    }
-
-    int getsumCost() {
-        return this.totalCost;
-    }
     void updateProduct(int price) {
     	
     	this.initialProduct = this.initialProduct+1;
